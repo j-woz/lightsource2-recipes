a521bcdea351f14c21ccb473939a1115627cfcff
Justin Wozniak <woz@anl.gov>
2023-02-06 08:45:02 -0600

    Working

--- recipes-tag/swift-t/conda-install.sh
+++ recipes-tag/swift-t/conda-install.sh
@@ -3,8 +3,11 @@ set -eux
 
 which conda
 
+# Install these before swift-t:
 conda install -c conda-forge gcc
-# conda install -c conda-forge openjdk=11
-# conda install -c conda-forge openmpi
+conda install -c conda-forge openjdk=11
+conda install -c conda-forge openmpi
+# If needed:
+conda install -c conda-forge jupyter
 
-conda install -c conda-forge /home/woz/Public/sfw/Anaconda/conda-bld/linux-64/swift-t-1.6.0-py39_1.tar.bz2
+conda install -c conda-forge /home/woz/sfw/Anaconda/conda-bld/linux-64/swift-t-1.6.0-py39_1.tar.bz2

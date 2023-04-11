d0160277ef4d10805c4f4bde1651ae2aec81bb58
Justin Wozniak <woz@anl.gov>
2023-04-11 15:57:38 -0500

    Working builder for basic Swift/T+Python

--- recipes-tag/swift-t/linux-64/conda-build.sh
+++ recipes-tag/swift-t/linux-64/conda-build.sh
@@ -1,15 +1,41 @@
-#!/bin/bash
+#!/bin/bash -norc
 set -eu
 
-set -x
+LOG=conda-build.log
+if [[ -f $LOG ]]
+then
+  mv -v --backup=numbered $LOG $LOG.bak
+  echo
+fi
+
 # mkdir -pv out
-which conda
-conda build \
-      -c conda-forge \
-      --debug \
-      --dirty \
-      --no-remove-work-dir \
-      .
+
+(
+  echo "using conda:"
+  which conda
+  echo
+
+  set -x
+  conda build \
+        -c conda-forge \
+        --dirty \
+        .
+) |& tee $LOG
+echo
+echo "conda build succeeded."
+echo
+
+LINES=( $( grep -A 1 "anaconda upload" $LOG ) )
+FILE=${LINES[-1]}
+
+(
+  echo
+  echo md5sum: $( md5sum $FILE )
+) | tee --append $LOG
+
+# --debug
+
+#      --no-remove-work-dir
 
 # --bootstrap NAME
 

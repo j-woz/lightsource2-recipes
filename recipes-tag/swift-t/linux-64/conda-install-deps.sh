7ce75964cac9e6b2f985bdfca0116b7b050c7840
Justin Wozniak <woz@anl.gov>
2023-04-11 15:57:26 -0500

    Adding linux-64/conda-install-deps.sh

---
+++ recipes-tag/swift-t/linux-64/conda-install-deps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+# CONDA INSTALL DEPS
+# Test to just install dependencies
+# Keep in sync with meta.yaml
+
+which conda
+conda env list
+
+set -x
+conda install -c conda-forge \
+      ant \
+      autoconf \
+      gcc \
+      make \
+      openjdk \
+      mpich-mpicc \
+      python \
+      swig \
+      zsh \
